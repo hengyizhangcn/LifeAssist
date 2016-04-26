9b315414f1460fca0cf5c3e86166502f5a64c2c3
hengyi.zhang@husor.com <hengyi.zhang@husor.com>
2016-04-26 20:54:48 +0800

    first commit

---
+++ LifeAssist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifeAssist
+//
+//  Created by zhy on 4/17/16.
+//  Copyright © 2016 lifes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
